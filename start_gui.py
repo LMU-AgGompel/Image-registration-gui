5e16d73374996066a257419863a71e5db092f02c
Stefano Ceolin <stefanoceo@gmail.com>
2022-03-28 17:48:00 +0200

    first commit of the image annotation gui

---
+++ start_gui.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 25 18:13:00 2022
+
+@author: ceolin
+"""
+
+import image_annotation_gui as gui
+
+gui.start(main_window_size = (1400,1100), graph_canvas_width = 700)
