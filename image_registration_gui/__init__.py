a9e4fdb18b082c5f3be06bc522c4f238edd594c0
Stefano Ceolin <stefanoceo@gmail.com>
2022-06-10 14:59:06 +0200

    renamed module and functions to image_registration_gui

---
+++ image_registration_gui/__init__.py
@@ -0,0 +1,14 @@
+# __init__.py
+
+"""
+ag_gompel_image_annotation_gui.
+
+A graphical interface to annotate and place landmarks on images, built with the PySimpleGUI TKinter framework.
+
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Stefano Ceolin'
+
+
+from image_registration_gui.gui import start_image_registration_GUI as start
